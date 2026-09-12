fbbcf6e931470700084fbc7d415995f9e80dfe94
Damian Zamudio <274949844+iNavn@users.noreply.github.com>
2026-09-12 06:02:55 -0600

    chore: remove Module 0 debugger practice endpoints (ping, saludos) and default Hello World

--- src/Vault.Api/Program.cs
+++ src/Vault.Api/Program.cs
@@ -10,28 +10,6 @@ builder.Services.AddSingleton<ICategoryRepository, InMemoryCategoryRepository>()
 
 var app = builder.Build();
 
-app.MapGet("/ping", saludoConFecha);
-static string saludoConFecha()
-{
-    DateTime fechaUTC = DateTime.UtcNow;
-    return $"Hola la fecha y hora actual es: {fechaUTC}";
-}
-
-app.MapGet("/saludos/{n:int}", GenerarSaludos);
-static string GenerarSaludos(int n)
-{
-    var resultado = "";
-    for (int i = 0; i < n; i++)
-    {
-        resultado += ConstruirSaludo(i);
-    }
-    return resultado;
-}
-static string ConstruirSaludo(int indice)
-{
-    return $"Saludo #{indice} generado a las {DateTime.UtcNow:HH:mm:ss}\n";
-}
-
 app.MapVaultItemEndpoints();
 app.MapCategoryEndpoints();
 
